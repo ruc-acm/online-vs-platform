3b7ced8373810a180957672db62d46b6d83763d0
Indeed <indeed@indeedblog.net>
2014-07-01 14:35:10 +0800

    add constants about illegal ouput status.

--- website/common/models/ExecutionRecord.php
+++ website/common/models/ExecutionRecord.php
@@ -25,9 +25,11 @@ class ExecutionRecord extends ActiveRecord
     const STATUS_FINISHED = 2;
     const STATUS_RUNTIME_ERROR = 3;
     const STATUS_TLE = 4;
+    const STATUS_ILLEGAL_MOVE = 5;
+    const STATUS_BAD_FORMAT = 6;
     const STATUS_INTERNAL_ERROR = -1;
     const STATUS_REJECTED = -2;
-
+    
     const WINNER_ATTACKER = 1;
     const WINNER_DEFENDER = 2;
 
@@ -36,7 +38,7 @@ class ExecutionRecord extends ActiveRecord
      */
     public static function tableName()
     {
-        return 'executionRecord';
+        return '{{%executionRecord}}';
     }
 
     /**
