e453646c41dd0cf848e03e86b8cbe9671801a46c
Indeed <indeed@indeedblog.net>
2014-07-02 00:42:42 +0800

    judge-client WIP.

--- judge/judge-client.py
+++ judge/judge-client.py
@@ -12,7 +12,7 @@ class UserProgram:
         self.process = None
         self.timeout = 1.0
         self.timer = None
-        self.alive = True
+        self.tle = False
 
     def execute(self):
         if not self.path:
@@ -20,7 +20,8 @@ class UserProgram:
         self.process = Popen(self.path, stdin=PIPE, stdout=PIPE, stderr=PIPE)
 
     def start_timer(self):
-        self.timer = Timer(self.timeout, lambda: self.kill_process())
+        self.timer = Timer(self.timeout, lambda: self.time_limit_exceeded())
+        self.timer.start()
 
     def stop_timer(self):
         self.timer.cancel()
@@ -33,19 +34,46 @@ class UserProgram:
         self.process.stdout.write(line)
 
     def kill_process(self):
-        self.process.kill()
-        self.alive = False
+        try:
+            self.process.kill()
+        except Exception:
+            pass
+
+    def time_limit_exceeded(self):
+        self.tle = True
+        self.kill_process()
 
 
 class Match:
     def __init__(self):
         self.started = False
         self.winner = None
+        self.reason = None
 
     def end(self, winner):
+        global attacker, defender
         self.winner = winner
-        print "Competition ended and the winner is %s"%winner
+        print >> sys.stderr, "Competition ended and the winner is %s" % winner
+        print >> sys.stderr, "Reason is %s" % self.reason
+        attacker.kill_process()
+        defender.kill_process()
         quit()
+
+    def time_limit_exceeded(self):
+        self.reason = "TLE"
+
+    def defeat(self, return_code=1):
+        if return_code == 1:
+            self.reason = "Finished"
+        elif return_code == 2:
+            self.reason = "Illegal Movement"
+        elif return_code == 3:
+            self.reason = "Illegal Output"
+        else:
+            self.reason = "Unknown"
+
+    def crashed(self):
+        self.reason = "Crashed"
 
 
 match = Match()
@@ -57,22 +85,29 @@ defender_judge = Judge("defender")
 
 
 def run_step(attacker, attacker_judge, defender, defender_judge):
+    global match
     try:
         result = attacker_judge.before_write()
         if result:
+            match.defeat(result)
             match.end(attacker_judge.player_name if attacker_judge.victorious() else defender_judge.player_name)
         lines = attacker_judge.on_write()
         for line in lines:
             print >> attacker.process.stdin, line
         attacker.start_timer()
         line = attacker.read_line()
+        if not line:
+            if attacker.process.poll():
+                raise Exception('Process ended.')
         attacker.stop_timer()
         result = attacker_judge.after_read(line.strip())
         if result:
+            match.defeat(result)
             match.end(attacker_judge.player_name if attacker_judge.victorious() else defender_judge.player_name)
     except Exception as e:
-        attacker.kill_process()
-        raise e
+        match.crashed() if attacker.process.returncode != -9 else match.time_limit_exceeded()
+        match.end(defender_judge.player_name)
+
 
 attacker.execute()
 defender.execute()
