f1047b77e054f73475eb18cb10979bd8c587f218
Indeed <indeed@indeedblog.net>
2014-07-03 13:06:59 +0800

    fix schema of table game.

--- website/console/migrations/m130524_201442_init.php
+++ website/console/migrations/m130524_201442_init.php
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS executionRecord (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS game (
-  id int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   displayName varchar(255) NOT NULL,
   PRIMARY KEY (id)
