9eefdcca1a422b19c108e4871929d6afb434d635
Indeed <indeed@indeed.moe>
2015-07-07 23:26:17 +0800

    fix dependency not loaded.

--- website/frontend/replay/views/blokus/replay.php
+++ website/frontend/replay/views/blokus/replay.php
@@ -8,7 +8,7 @@
 $this->title = 'Replay - #' . $model->id;
 $this->params['breadcrumbs'][] = ['label' => 'Status', 'url' => ['competition/status']];
 $this->params['breadcrumbs'][] = $this->title;
-$this->registerJsFile('js/show_replay.js');
+$this->registerJsFile('js/show_replay.js', ['depends' => ['frontend\assets\AppAsset']]);
 ?>
 <div class="competition-replay-<?= $model->id ?>">
     <a href="<?= $url ?>" class="btn btn-danger">Go to external replay player.</a>
