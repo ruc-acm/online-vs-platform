9571643b87c0745aec37b4c5fb2427ae928636b9
Indeed <indeed@indeed.moe>
2015-07-07 23:23:47 +0800

    fix script loading method & clean up.

--- website/frontend/replay/views/blokus/replay.php
+++ website/frontend/replay/views/blokus/replay.php
@@ -8,15 +8,8 @@
 $this->title = 'Replay - #' . $model->id;
 $this->params['breadcrumbs'][] = ['label' => 'Status', 'url' => ['competition/status']];
 $this->params['breadcrumbs'][] = $this->title;
+$this->registerJsFile('js/show_replay.js');
 ?>
 <div class="competition-replay-<?= $model->id ?>">
-    <h1>Warning</h1>
-
-    <p>The page you are going to visit may contain certain content that is not suitable for you to view. And because of
-        the fact that the destination page is outside this website, we have no control with it.</p>
-
-    <p>If you really want to proceed, please click the button below.</p>
-    <a href="<?= $url ?>" class="btn btn-danger">I understand, please let me see it.</a>
-    <button onclick="history.back();" class="btn btn-primary">No, let me back.</button>
+    <a href="<?= $url ?>" class="btn btn-danger">Go to external replay player.</a>
 </div>
-<script src="js/show_replay.js" ></script>
